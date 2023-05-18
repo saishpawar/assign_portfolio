ee51b784503323e2c5f3fc1cbea5ba074e6c80d3
Saish Pawar <saish.pawar@hbwsl.com>
2023-05-18 11:05:27 +0530

    done1

--- script.js
+++ script.js
@@ -1,20 +1,34 @@
-let click=document.getElementById("ham");
-let hamopt=document.getElementById('hammenu');
+// let click=document.getElementById("ham");
+// let hamopt=document.getElementById('hammenu');
 
-let c=0;
+// let c=0;
+
+// console.log("before click "+c);
+// function clicked(){
+//     if(c==0){
+//         hamopt.style.display="block";
+//         c=1;
+//     }
+//     else{
+//         hamopt.style.display="none";
+//         c=0;
+//     }
+
+//     console.log("after click "+c);
+// }
+
+
+let hamButton = document.getElementById("ham");
+
+let c=false;
 
 console.log("before click "+c);
 function clicked(){
-    if(c==0){
-        hamopt.style.display="block";
-        c=1;
-    }
-    else{
-        hamopt.style.display="none";
-        c=0;
-    }
-
-    console.log("after click "+c);
+    c?document.getElementById("hammenu").classList.remove("hammenuOpen"):document.getElementById("hammenu").classList.add("hammenuOpen");
+    c=!c;
 }
+
+
+
 
 
